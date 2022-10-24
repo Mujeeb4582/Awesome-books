ce35709b79c4005079036242a60895629f0cfecd
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-24 20:37:47 +0500

    Add the clearfunction to add js file

--- scriptv2.js
+++ scriptv2.js
@@ -1,49 +1,50 @@
+const booksList = []; // Declear array for storing the bookList
 
 class BookInfo {
+  // create the object using constructor method
   constructor(title, author) {
     this.title = title;
     this.author = author;
   }
 }
 
-// function Add(title, author){
-//   const book = new BookInfo(title, author);
-//   booksList.push(book);
-// }
-
 class UI {
-    static DisplayBooks() { 
-   const booksList = [];
-  const books = BookInfo;
-  
-  books.forEach((book) =>UI.addBookToList(book))
+  // To display the title and name of the author on the browser
+  static DisplayBooks() {
+    const books = BookInfo;
+
+    books.forEach((book) => UI.addBookToList(book));
   }
-  
+
   static addBookToList(book) {
     const books_list = document.getElementById('books_list');
-      books_list.innerHTML += `
-    <h3>Title ${book.title}</h3>
-    <h3>Auther ${book.author}</h3>
+    books_list.innerHTML += `
+    <h3>${book.title}</h3>
+    <h3>${book.author}</h3>
     <button type="submit">Remove</button>
     <hr>
     `;
   }
 }
 
-document.addEventListener("DOMContinentLoaded", UI.DisplayBooks)
+// clear the input field
+function clearInput() {
+  document.getElementById('bookInfo').reset();
+}
 
- document.querySelector('.bookInfo').addEventListener ("submit" , (e) =>  {
+document.addEventListener('DOMContinentLoaded', UI.DisplayBooks); // eventListner
 
- e.preventDefault();
+document.querySelector('.bookInfo').addEventListener('submit', (e) => {
+  // for loop for reading input field
+  e.preventDefault();
 
+  const Title = document.querySelector('.title').value;
+  const Author = document.querySelector('.author').value;
 
-    const Title = document.querySelector('.title').value;
-    const Author = document.querySelector('.author').value;
-    // const newtitle = Title
-    // const newauthor = Author
+  const book = new BookInfo(Title, Author); // creating the new object
+  booksList.push(book); // storing in the array
+  console.log(booksList);
 
-const book = new BookInfo(Title,Author);
-
-UI.addBookToList(book)
-
+  UI.addBookToList(book); // call the function for display the objects on the browser
+  clearInput(); // function call of clearing the input field
 });
