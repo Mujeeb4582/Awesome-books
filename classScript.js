f062cf887f84ed8c6bd878028eb5b0559d3decb7
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-25 20:27:26 +0500

    Add new Js file and create class methods

---
+++ classScript.js
@@ -0,0 +1,88 @@
+let booksList = []; // Declare a array for storing the bookList
+
+// grab all elements
+const addBookForm = document.querySelector('#bookInfo');
+const list = document.querySelector('.books_list');
+
+// make object instance and its methods
+class BookInfo {
+  // create the object using constructor method
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  createListItem(booksListIndex) {
+    const listWrapper = document.createElement('div');
+    listWrapper.className = 'listWrapper';
+    const unOrderList = document.createElement('ul');
+    const li1 = document.createElement('li');
+    const li2 = document.createElement('li');
+    const li3 = document.createElement('li');
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.type = 'button';
+    const horzintalLine = document.createElement('hr');
+
+    list.appendChild(listWrapper);
+    listWrapper.appendChild(unOrderList);
+    // set innerHTML value
+    li1.innerHTML = this.title;
+    li2.innerHTML = this.author;
+    li3.innerHTML = 'by';
+    unOrderList.appendChild(li1);
+    unOrderList.appendChild(li3);
+    unOrderList.appendChild(li2);
+    unOrderList.appendChild(removeBtn);
+    unOrderList.appendChild(horzintalLine);
+
+    // remove button click listner
+    removeBtn.addEventListener('click', (event) => {
+    // Remove item from DOM
+      event.target.parentElement.remove();
+      // Remove item from booksList Array
+      booksList.splice(booksListIndex, 1);
+      // Remove iem from localStorage
+      localStorage.setItem('list', JSON.stringify(booksList));
+    });
+  }
+}
+
+// Loop over Array, create the element and append to the DOM
+function renderBookList(lists) {
+  lists.forEach((element, index) => {
+    const book = new BookInfo(element.title, element.author);
+    book.createListItem(element, index);
+  });
+}
+
+// check is booksList exist in localStorage
+const storedBookList = localStorage.getItem('list');
+// //if Exist
+if (storedBookList) {
+  // JSON.parse booksList
+  const previousList = JSON.parse(storedBookList);
+  booksList = previousList;
+  renderBookList(booksList);
+}
+
+// form part
+addBookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Access input tag values
+  const bookName = addBookForm.bookTitle.value;
+  const bookAuthorName = addBookForm.bookAuthor.value;
+
+  const book = new BookInfo(bookName, bookAuthorName);
+
+  // Add object to the array
+  booksList.push(book);
+  book.createListItem();
+
+  // clear the input field on browser
+  addBookForm.bookTitle.value = '';
+  addBookForm.bookAuthor.value = '';
+  // localstorage
+  localStorage.setItem('list', JSON.stringify(booksList));
+});
